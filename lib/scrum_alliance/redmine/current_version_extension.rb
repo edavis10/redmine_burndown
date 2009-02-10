2b854ff08dc9881c6ed1b2916060c522e9361195
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-09 20:12:14 -0800

    Prevent a nil exception when a Version doesn't have an effective_date
    Version#effective_date is an optional attribute so it needs to be checked for
    nil before comparing.

--- lib/scrum_alliance/redmine/current_version_extension.rb
+++ lib/scrum_alliance/redmine/current_version_extension.rb
@@ -2,7 +2,7 @@ module ScrumAlliance
   module Redmine
     module CurrentVersionExtension
       def current_version
-        versions.detect {|version| version.created_on.to_date <= Date.current && version.effective_date >= Date.current }
+        versions.detect {|version| version.created_on.to_date <= Date.current && !version.effective_date.nil? && version.effective_date >= Date.current }
       end
     end # CurrentVersionExtension
   end # Redmine
